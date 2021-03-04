0f682fd51fdd8074e7989b199fdfa40297b9d4d1
Justin Edwards <justinledwards@gmail.com>
2021-03-04 10:36:13 -0500

    dramatic effect of regex

--- regex_learning.rb
+++ regex_learning.rb
@@ -2,9 +2,10 @@ browser_input = "416  test".split('')
 
 browser_input.each {|char| puts "Current character is: #{char}" }
 
-sleep(2)
+sleep(0.5)
 
 browser_input.each do |char| 
     puts "Character _#{char}_ is a number" if /\d/ === char
     puts "Character _#{char}_ is not a number" if /\D/ === char
+    sleep(0.2)
 end
